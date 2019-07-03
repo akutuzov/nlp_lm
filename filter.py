5f81fc75d0d826637502c1b22e995c1130d57f2d
akutuzov <akutuzov72@gmail.com>
2019-07-03 21:00:18 +0200

    Filtering script now compatible with smart_open

--- filter.py
+++ filter.py
@@ -1,13 +1,21 @@
 #! python3
 # coding: utf-8
 
-import sys
+import argparse
 import string
+from smart_open import open
 
-for line in sys.stdin:
-    res = line.strip()
-    res = ''.join([char for char in res if char.isalpha() or char in string.punctuation
-                   or char == ' ' or char == "'"])
-    res = res.strip()
-    if res:
-        print(res)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--in_corpus', '-c', help="Path to the input corpus", required=True)
+    parser.add_argument('--out_corpus', '-o', help="Path to the cleaned corpus", required=True)
+    args = parser.parse_args()
+
+    with open(args.out_corpus, 'a') as out:
+        for line in open(args.in_corpus, 'r'):
+            res = line.strip()
+            res = ''.join([char for char in res if char.isalpha() or char in string.punctuation
+                           or char == ' ' or char == "'"])
+            res = res.strip()
+            if res:
+                out.write(res + '\n')
